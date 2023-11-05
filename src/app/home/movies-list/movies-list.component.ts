9c8f54ce7f9e619290b3e850c8aa6870362f2941
A13DO <abdoalaa2146@gmail.com>
2023-11-05 12:19:02 +0200

    swiper modified

--- src/app/home/movies-list/movies-list.component.ts
+++ src/app/home/movies-list/movies-list.component.ts
@@ -29,7 +29,7 @@ export class MoviesListComponent implements OnInit, OnDestroy {
   //  - choose the detalis we need to be able to add movies wit my data Movie[]
   //  -
   //  -
-    @ViewChild('moviesList')
+  @ViewChild('moviesList')
   moviesList!: ElementRef;
 
   fetchMovies(event: Event) {
@@ -38,16 +38,11 @@ export class MoviesListComponent implements OnInit, OnDestroy {
     this.moviesList.nativeElement.scrollTop = this.moviesList.nativeElement.scrollHeight;
   }
   ngOnInit(): void {
-    let scrollMovie = document.getElementById("");
-    // window.addEventListener('scroll', function() {
-    //   // Get the current scroll position
-    //   var scrollRate = document.documentElement.scrollTop;
 
-    //   // Log the scroll position in the developer console
-    //   console.log('Scroll position:', scrollRate);
-    // });
-    // this.movies.push()
-    // =============== Trending Movies =================
+    console.log(this.backdropDiv);
+
+    // this.backdropDiv = ;
+    let scrollMovie = document.getElementById("");
     this.moviesRequests.getTrendingMovies()
     .subscribe(
       data => {
@@ -67,48 +62,41 @@ export class MoviesListComponent implements OnInit, OnDestroy {
     )
     this.getTopRatedMoviess(this.topRatedPageNum)
     this.backdropDiv = document.querySelector(".backdrop") as HTMLElement;
-
+  }
+  changeBackdrop(e: any) {
+    if (e) {
+      this.backdropDiv.style.backgroundImage = `url(${e})`;
     }
-    showImage(event: Event) {
-      let imgDiv = ((event.target as HTMLElement).children[0].getElementsByClassName("poster")[0]) as HTMLElement;
-      // assign poster
-      this.trendingMoives.find( (movie) => {
-        if (movie.posterimagePath == imgDiv.getAttribute("src")) {
-          // backgroundImage shloud be backdropDiv.url
-
-          this.backdropDiv.style.backgroundImage = `url(${movie.backdropimagePath})`;
-        }
-      });
-    }
-    onMouseLeave() {
+  }
+  onMouseLeave() {
       this.backdropDiv.style.backgroundImage = `url("https://wallpapers.com/images/hd/mysterious-officer-k-blade-runner-2049-4k-ugzq0o6fnr4xsfnw.jpg")`;
-    }
-    getTopRatedMoviess(topRatedPageNum: number) {
+  }
+  getTopRatedMoviess(topRatedPageNum: number) {
       this.moviesRequests.getTopRatedMovies(topRatedPageNum) // pass page number
-      .subscribe(
+    .subscribe(
         data => {
           for (let movie of data.results) {
             this.topRatedMoives.push(
               {
               name: movie.title,
-              id: movie.id,
-              overview: movie.overview,
-              year: (new Date(movie.release_date)).getFullYear().toString(),
-              posterimagePath: this.link + movie.poster_path,
-              rating: movie.vote_average // need edit
-              }
-            )
-          }
+            id: movie.id,
+            overview: movie.overview,
+            year: (new Date(movie.release_date)).getFullYear().toString(),
+            posterimagePath: this.link + movie.poster_path,
+            rating: movie.vote_average // need edit
+            }
+          )
+        }
         }
       )
     }
-    topRatedPageOne() {
-      this.topRatedPageNum = 1;
-      console.log(this.topRatedPageNum)
-      this.topRatedMoives = [];
-      this.getTopRatedMoviess(this.topRatedPageNum)
-      // window.scrollTo({
-			// 	top: 619,
+  topRatedPageOne() {
+    this.topRatedPageNum = 1;
+    console.log(this.topRatedPageNum)
+    this.topRatedMoives = [];
+    this.getTopRatedMoviess(this.topRatedPageNum)
+    // window.scrollTo({
+		// 	top: 619,
 			// 	behavior: 'auto'
 			// })
     }
@@ -129,46 +117,27 @@ export class MoviesListComponent implements OnInit, OnDestroy {
       console.log("Before")
       // window.scrollTo(0, this.scrollPosition);
       this.getTopRatedMoviess(this.topRatedPageNum)
-      // console.log("After")
-      // var scrollRate = document.documentElement.scrollTop;
-      // Get the current position of the scroll bar
-
-      // Refresh the page
-
-      // Set the scroll position back to where it was before refreshing
       }
-    topRatedPageFour(event: Event) {
-      event.preventDefault();
-      this.topRatedPageNum = 4;
-      console.log(this.topRatedPageNum)
-      this.topRatedMoives = [];
-      var scrollRate = document.documentElement.scrollTop;
-      this.getTopRatedMoviess(this.topRatedPageNum)
-      // if (scrollRate == 619 || scrollRate !== 619) {
-      //   window.scrollTo({
-      //     top: 619,
-      //     behavior: 'auto'
-      //   })
-      // }
-      // your code to fetch movies from API and display them
-      this.moviesList.nativeElement.scrollTop = this.moviesList.nativeElement.scrollHeight;
-
-    }
+  topRatedPageFour(event: Event) {
+    event.preventDefault();
+    this.topRatedPageNum = 4;
+    console.log(this.topRatedPageNum)
+    this.topRatedMoives = [];
+    var scrollRate = document.documentElement.scrollTop;
+    this.getTopRatedMoviess(this.topRatedPageNum)
+    // your code to fetch movies from API and display them
+    this.moviesList.nativeElement.scrollTop = this.moviesList.nativeElement.scrollHeight;
+  }
 
 
-
-
-
-
-    pageNum: number = 0;
-    slideLeft() {
-    if (this.pageNum >= 1) {
-      this.pageNum -= 1;
-      this.getSlideValue()
-      console.log(this.pageNum)
-      console.log("LEFT")
-    }
-
+  pageNum: number = 0;
+  slideLeft() {
+  if (this.pageNum >= 1) {
+    this.pageNum -= 1;
+    this.getSlideValue()
+    console.log(this.pageNum)
+    console.log("LEFT")
+  }
   }
   slideRight() {
     if (this.pageNum < 3) {
--- src/app/swiper/swiper.component.ts
+++ src/app/swiper/swiper.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Swiper } from 'swiper';
 // core version + navigation, pagination modules:
 // import function to register Swiper custom elements
@@ -13,9 +13,29 @@ register();
   styleUrls: ['./swiper.component.css']
 })
 export class SwiperComponent implements OnInit {
+
+
   SliderMovies!: Movie[];
   @Input() movies!: Movie[];
+  @Output() backdrop: EventEmitter<any> = new EventEmitter();
+  @Output() defaultBackdrop: EventEmitter<any> = new EventEmitter();
+  // backdropDiv: any;
   ngOnInit(): void {
     this.SliderMovies = this.movies;
+  }
+  showImage(event: Event) {
+    let imgDiv = ((event.target as HTMLElement).children[0].getElementsByClassName("poster")[0]) as HTMLElement;
+    // assign poster
+    this.SliderMovies.find( (movie) => {
+      if (movie.posterimagePath == imgDiv.getAttribute("src")) {
+        console.log(imgDiv.getAttribute("src"));
+        // backgroundImage shloud be backdropDiv.url
+        this.backdrop.emit(movie.backdropimagePath)
+        // this.backdrop.style.backgroundImage = `url(${movie.backdropimagePath})`;
+      }
+    });
+  }
+  onMouseLeave() {
+    this.defaultBackdrop.emit()
   }
 }
