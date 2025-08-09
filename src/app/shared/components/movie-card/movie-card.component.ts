2f0ae999b407c84e94280377ab3f4049c5b58b78
A13DO <abdoalaa2146@gmail.com>
2025-08-09 22:56:19 +0200

    v1.2

--- src/app/shared/components/movie-card/movie-card.component.ts
+++ src/app/shared/components/movie-card/movie-card.component.ts
@@ -1,11 +1,19 @@
 import { LinksService } from '../../../core/services/links.service';
 import { MoviesRequestsService } from '../../../core/services/movies-requests.service';
-import { Component, Input, OnInit, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnDestroy,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { Movie } from 'src/app/core/interfaces/movie.module';
 import { MoviesListComponent } from '../../../pages/home/movies-list/movies-list.component';
 import { Subscription } from 'rxjs';
 import { faEye } from '@fortawesome/free-regular-svg-icons';
-import { faHeart, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faStar, faPlus } from '@fortawesome/free-solid-svg-icons';
+
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { jwtDecode } from 'jwt-decode';
@@ -13,9 +21,9 @@ import { jwtDecode } from 'jwt-decode';
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.css']
+  styleUrls: ['./movie-card.component.css'],
 })
-export class MovieCardComponent implements OnInit, OnChanges, OnDestroy{
+export class MovieCardComponent implements OnInit, OnChanges, OnDestroy {
   @Input() movie!: Movie;
   @Input() componentName!: string;
   // with using *ngIf if the value is true the button will appear
@@ -30,11 +38,12 @@ export class MovieCardComponent implements OnInit, OnChanges, OnDestroy{
   faAdd = faPlus;
   faLove = faHeart;
   faEye = faEye;
+  faStar = faStar;
   constructor(
     private router: Router,
     private requestService: MoviesRequestsService,
     private linksService: LinksService
-    ){}
+  ) {}
   // Firebase add movies.json to add file
   startTime: any;
   endTime: any;
@@ -48,7 +57,7 @@ export class MovieCardComponent implements OnInit, OnChanges, OnDestroy{
   favoriteToggleClass: boolean = false;
   watchlistToggleClass: boolean = false;
   checkIfNotSignedIn() {
-    let isSignedIn = window.localStorage.getItem("isSignedIn");
+    let isSignedIn = window.localStorage.getItem('isSignedIn');
     if (!isSignedIn) {
       this.watchedToggleClass = false;
       this.watchlistToggleClass = false;
@@ -66,39 +75,39 @@ export class MovieCardComponent implements OnInit, OnChanges, OnDestroy{
     if (this.watchlistStatus == true) {
       this.watchlistToggleClass = true;
     }
-    this.checkIfNotSignedIn()
+    this.checkIfNotSignedIn();
   }
   ngOnChanges(changes: SimpleChanges) {
     // Called whenever an input property changes
     for (const propName in changes) {
-      if (propName == "watchedStatus") {
+      if (propName == 'watchedStatus') {
         if (changes[propName].currentValue == true) {
           this.watchedToggleClass = true;
         }
-      } else if (propName == "favoriteStatus") {
+      } else if (propName == 'favoriteStatus') {
         if (changes[propName].currentValue == true) {
           this.favoriteToggleClass = true;
         }
-      } else if (propName == "watchlistStatus") {
+      } else if (propName == 'watchlistStatus') {
         if (changes[propName].currentValue == true) {
           this.watchlistToggleClass = true;
         }
       }
     }
-  this.checkIfNotSignedIn()
+    this.checkIfNotSignedIn();
   }
 
-  mySub: Subscription = new Subscription;
+  mySub: Subscription = new Subscription();
   savedMovies: Movie[] = [];
-  WATCHED = "watched"
-  WATCHLIST = "watchlist"
-  FAVORITE = "favorite"
+  WATCHED = 'watched';
+  WATCHLIST = 'watchlist';
+  FAVORITE = 'favorite';
 
   onAddToWatched(movie: Movie) {
-    let isSignedIn = window.localStorage.getItem("isSignedIn");
-    if (isSignedIn == "true") {
+    let isSignedIn = window.localStorage.getItem('isSignedIn');
+    if (isSignedIn == 'true') {
       // movie.isWatched = true;
-      const componentName = "watched";
+      const componentName = 'watched';
       // toggle
       if (this.watchedToggleClass) {
         this.watchedToggleClass = false;
@@ -109,32 +118,36 @@ export class MovieCardComponent implements OnInit, OnChanges, OnDestroy{
         this.requestService.saveMovies(movie, this.watchedUrl, this.WATCHED);
       }
     } else if (!isSignedIn) {
-      this.router.navigate(["/login"]);
+      this.router.navigate(['/login']);
     }
   }
   onAddToWatchlist(movie: Movie) {
-    let isSignedIn = window.localStorage.getItem("isSignedIn");
-    if (isSignedIn == "true") {
+    let isSignedIn = window.localStorage.getItem('isSignedIn');
+    if (isSignedIn == 'true') {
       movie.isWatchList = true;
-      const componentName = "watchlist";
+      const componentName = 'watchlist';
       // toggle
       if (this.watchlistToggleClass) {
         this.watchlistToggleClass = false;
         this.requestService.deleteMovie(movie, componentName);
-      } else if (this.watchlistToggleClass == false){
+      } else if (this.watchlistToggleClass == false) {
         this.watchlistToggleClass = true;
         // Send Http Request
-        this.requestService.saveMovies(movie, this.watchlistUrl, this.WATCHLIST);
+        this.requestService.saveMovies(
+          movie,
+          this.watchlistUrl,
+          this.WATCHLIST
+        );
       }
     } else if (!isSignedIn) {
-      this.router.navigate(["/login"]);
+      this.router.navigate(['/login']);
     }
   }
   onAddToFavorites(movie: Movie) {
-    let isSignedIn = window.localStorage.getItem("isSignedIn");
-    if (isSignedIn == "true") {
+    let isSignedIn = window.localStorage.getItem('isSignedIn');
+    if (isSignedIn == 'true') {
       movie.isFavorite = true;
-      const componentName = "favorite";
+      const componentName = 'favorite';
       // toggle
       if (this.favoriteToggleClass) {
         this.favoriteToggleClass = false;
@@ -144,31 +157,32 @@ export class MovieCardComponent implements OnInit, OnChanges, OnDestroy{
         this.requestService.saveMovies(movie, this.favoritesUrl, this.FAVORITE);
       }
     } else if (!isSignedIn) {
-      this.router.navigate(["/login"]);
+      this.router.navigate(['/login']);
     }
   }
   deleteMovie(movie: Movie, componentName: string) {
-    console.log(movie)
-    console.log("Component Name: " + componentName)
+    console.log(movie);
+    console.log('Component Name: ' + componentName);
     // remove from
-    this.requestService.deleteMovie(movie, componentName)
+    this.requestService.deleteMovie(movie, componentName);
 
     // remove from page
-    document.addEventListener("click", function (e) {
+    document.addEventListener('click', function (e) {
       if ((e.target as HTMLElement).classList.contains('delete-button')) {
         const movieCardEl = (e.target as HTMLElement).closest('.movie-card');
-        var disElClasses = (movieCardEl?.childNodes[3] as HTMLElement).classList;
-        disElClasses.add("disable")
-          console.log((movieCardEl?.childNodes[3] as HTMLElement).classList)
+        var disElClasses = (movieCardEl?.childNodes[3] as HTMLElement)
+          .classList;
+        disElClasses.add('disable');
+        console.log((movieCardEl?.childNodes[3] as HTMLElement).classList);
       }
-    }
-      )
-    }
-    goToMoviePage() {
-      this.router.navigate(["movie", this.movie.name], {state: {id: this.movie.id}})
-    }
-    ngOnDestroy() {
-      this.mySub.unsubscribe()
-    }
+    });
+  }
+  goToMoviePage() {
+    this.router.navigate(['movie', this.movie.name], {
+      state: { id: this.movie.id },
+    });
+  }
+  ngOnDestroy() {
+    this.mySub.unsubscribe();
+  }
 }
-
